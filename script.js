5f48a195232f2f63239e43a739e765f802701fb2
srconsiglio <caioconsiglio@hotmail.com>
2022-08-29 10:17:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function setBlueBackground(){
+    let element = document.querySelector(".tittle") 
+    element.style.backgroundColor = "blue"
+}
+
+
+function setTransparenteBackground(){
+    let element = document.querySelector(".tittle")
+    element.style.backgroundColor = "transparent"
+}
+
+
+
+function setRedColor(){
+    let element = document.querySelector(".tittle")
+    element .style. color = "red"
+}
+
+function setNormalColor(){
+    let element = document.querySelector(".tittle")
+    element.style.color = "black"
+}
